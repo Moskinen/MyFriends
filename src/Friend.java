a94053f11dee2c96a4949465f0656bde8c84774e
Yakoob1884 <vivja.aty@gmail.com>
2025-03-07 15:14:18 +0100

    Friend class

--- src/Friend.java
+++ src/Friend.java
@@ -1,2 +1,26 @@
-public class Friend {
+public class Friend extends Person {
+    //Attributes
+    String name;
+    String phone;
+    String email;
+
+    // Constructor
+    public Friend(String name, String phone, String email) {
+        super(name, phone, email);
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPhone(){
+        return this.phone;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
 }
