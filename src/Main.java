8b24fbf2f5c09d853d4026aefbdbd3cb906f1221
Murray4 <sebastianmurraysimonsen@gmail.com>
2025-03-07 15:45:40 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
                 System.out.println("1");
                 break;
             case 2:
-                System.out.println("2");
+                enterNewFriend();
                 break;
             case 3:
                 System.out.println("3");
