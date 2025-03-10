e5b54f5348ef828177ab1d0757bccb5c2976796c
Yakoob1884 <vivja.aty@gmail.com>
2025-03-10 15:26:03 +0100

    FileHub update

--- src/FileHub.java
+++ src/FileHub.java
@@ -1,5 +1,37 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class FileHub {
+    public static void main(String[]args){
+        createFile();
+        writeToFile();
+    }
+
+    public static void createFile(){
+        try {
+            File friendList = new File("friendList.txt");
+            if (friendList.createNewFile()) {
+                System.out.println("File created: " + friendList.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeToFile(){
+        try {
+            FileWriter myWriter = new FileWriter("friendList.txt");
+            myWriter.write("So many things are happening, and i understand nothing!");
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
